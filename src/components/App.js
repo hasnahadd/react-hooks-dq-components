17f8c8130e7ee2903b3a0efc11f2151a272c532b
hasna <manelhasnahaddoud@gmail.com>
2023-09-03 15:24:40 +0100

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import AlbumCard from './AlbumCard'
+import AlbumList from './AlbumList'
+import GenrePicker from './GenrePicker'
+import Logo from './Logo'
+import MainContent from './MainContent'
+import MenuLinks from './MenuLinks'
+import Search from './Search'
+import SideMenu from './SideMenu'
+import TopMenu from './TopMenu'
+
+
+
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">{/* What components go here? */}
+  {/* <AlbumCard/> */}
+  <TopMenu/> 
+  <MainContent/>
+
+  <SideMenu/>
+  </div>;
+
+
+
+
 }
 
 export default App;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import AlbumCard from './AlbumCard'
+import AlbumList from './AlbumList'
+import GenrePicker from './GenrePicker'
+import MenuLinks from './MenuLinks'
+import TopMenu from './TopMenu'
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>{/* What components go here? */}
+  {/* <TopMenu/> */}
+  <GenrePicker/>
+  <AlbumList/>
+  
+  
+  
+  
+  </main>;
 }
 
 export default MainContent;
